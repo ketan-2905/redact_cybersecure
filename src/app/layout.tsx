ad67a8af730cdc4a5cd3cc8dd7cb8ce2def1e89e
ketan-2905 <ketangaikwad2905@gmail.com>
2025-11-24 13:01:35 +0530

    favicon error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-       <link rel="icon" type="image/x-icon" href="C:/Users/Ketan/Desktop/redact_cybersecure/frontend/public/favicon.ico"/>
+       <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
